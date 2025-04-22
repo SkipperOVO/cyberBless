d40a594627803d7c4d30f9c60dddcf327cc796fe
fields <zhangyc33@chinatelecom.cn>
2025-04-23 01:11:55 +0800

    完善香定位和燃烧逻辑

--- assets/scripts/burningController.ts
+++ assets/scripts/burningController.ts
@@ -13,17 +13,12 @@ const { ccclass, property } = _decorator;
 @ccclass('burningController')
 export class burningController extends Component {
 
-    @property({
-        type: CCBoolean,
-        tooltip: '是否燃烧',
-    })
     private _burning: boolean = false;
 
     @property({
         type: Node
     })
     private startIssueInsenseButtonNode : Node = null;
-
 
     private timeQuota: number = 60;
 
@@ -38,12 +33,14 @@ export class burningController extends Component {
        
     }
 
-    initState(insense: Insense) {
-        for (let i = 0; i < this.insense.length; i++) {
-            const ins = this.insense[i];
-            this.setBurningTime(ins.getBurningTime());
+    // 单支香
+    initStateSingle(insense: Insense) {
+            this.setBurningTime(insense.getBurningTime());
             this.node.setScale(this.node.scale.y, 1, this.node.scale.z);
             this.timeEplapsed = 0;
+
+            this.insense = []
+            this.node.removeAllChildren();
 
             resources.load(insense.getInsenseName() + '/spriteFrame', SpriteFrame, (err, spriteFrame) => {
                 if (err) {
@@ -56,12 +53,82 @@ export class burningController extends Component {
                 const sprite = spriteNode.addComponent(Sprite);
                 sprite.spriteFrame = spriteFrame;
 
-                spriteNode.setPosition(20*(i+1), 0, 0); // Set to the center
-                spriteNode.getComponent(UITransform).setAnchorPoint(0, 0); // Set size to 100x100
+                // 获取父节点的尺寸
+                // const parentTransform = this.node.getComponent(UITransform);
+                // const parentSize = parentTransform.contentSize;
+                // console.log("parentSize", parentSize);
+
+                // // 获取香炉的尺寸，也就是当前节点的尺寸
+                const burnerTransform = this.node.getComponent(UITransform);
+                const burnerSize = burnerTransform.contentSize;
+                console.log("burnerSize", burnerSize);
+                
+                // const burnerTransform = this.burnerNode.getComponent(UITransform);
+                // const burnerPosition = this.burnerNode.position;
+                // console.log("burnerPosition", burnerPosition);
+                // // 获取香炉的尺寸
+                // const burnerSize = burnerTransform.contentSize;
+                // console.log("burnerSize", burnerSize);
+
+                // 获取 spriteNode 香的尺寸
+                const spriteTransform = spriteNode.getComponent(UITransform);
+                const spriteSize = spriteTransform.contentSize;
+
+                spriteNode.setPosition(0-spriteSize.x/2, burnerSize.y/2 - 20, 0); // Set to the center
+
+                console.log("spriteNode position", spriteNode.position);
+
+                spriteNode.getComponent(UITransform).setAnchorPoint(0, 0); 
+                this.node.addChild(spriteNode);
+            });
+        
+    }
+
+
+    // 多支香
+    initStateMulti(insenses: Insense[]) {
+        this.insense = insenses;
+        this.setBurningTime(insenses[0].getBurningTime());
+        this.node.setScale(this.node.scale.y, 1, this.node.scale.z);
+        this.timeEplapsed = 0;
+        this.node.removeAllChildren();
+
+        for (let i = 0; i < insenses.length; i++) {
+            resources.load(insenses[i].getInsenseName() + '/spriteFrame', SpriteFrame, (err, spriteFrame) => {
+                if (err) {
+                    console.error('Failed to load sprite frame:', err);
+                    // Provide a fallback or notify the user
+                    return;
+                }
+
+                const spriteNode = new Node('BurningInsenseSprite');
+                const sprite = spriteNode.addComponent(Sprite);
+                sprite.spriteFrame = spriteFrame;
+
+                // 获取父节点的尺寸
+                const parentTransform = this.node.getComponent(UITransform);
+                const parentSize = parentTransform.contentSize;
+                console.log("parentSize", parentSize);
+
+                // 获取香炉的尺寸，也就是当前节点的尺寸
+                const burnerTransform = this.node.getComponent(UITransform);
+                const burnerSize = burnerTransform.contentSize;
+                console.log("burnerSize", burnerSize);
+
+                // 获取 spriteNode 香的尺寸
+                const spriteTransform = spriteNode.getComponent(UITransform);
+                const spriteSize = spriteTransform.contentSize;
+
+                spriteNode.setPosition(0-spriteSize.x/2, burnerSize.y/2 - 20, 0); // Set to the center
+
+                console.log("spriteNode position", spriteNode.position);
+
                 this.node.addChild(spriteNode);
             });
         }
-        
+        // this.node.setScale(this.node.scale.x, 1, this.node.scale.z);
+        // this.node.setPosition(this.node.position.x, this.node.position.y + 100, this.node.position.z);   
+
     }
 
     // todo 添加香在燃烧时，定期增加用户的功德值. ui 应该要和业务逻辑分离吗？
@@ -69,13 +136,17 @@ export class burningController extends Component {
         if (!this._burning) {
             return;
         }
+        
+        const insenseNode = this.node.getChildByName("BurningInsenseSprite");
 
         this.timeEplapsed += deltaTime;
         if (this.timeEplapsed <= this.timeQuota) {
             const progress = this.timeEplapsed / this.timeQuota;
-            this.node.setScale(this.node.scale.x, 1 - progress, this.node.scale.z);
+            insenseNode.setScale(insenseNode.scale.x, 1 - progress, insenseNode.scale.z);
+            // this.node.setScale(this.node.scale.x, 1 - progress, this.node.scale.z);
         } else {
-            this.node.setScale(this.node.scale.x, 0, this.node.scale.z);    
+            insenseNode.setScale(insenseNode.scale.x, 0, insenseNode.scale.z);
+            // this.node.setScale(this.node.scale.x, 0, this.node.scale.z);    
         }
 
         // this.node.setPosition(this.node.position.x + deltaTime * 50, this.node.position.y);
@@ -84,14 +155,16 @@ export class burningController extends Component {
         // this.node.setScale(this.node.scale.x, Math.max(newY, 0), this.node.scale.z);
         // this.node.setPosition(this.node.position.x, newY, this.node.position.z);
 
-        if (this.node.scale.y <= 0) {
+        if (insenseNode.scale.y <= 0) {
             this.stopBurn();
+            this.insense = [];
+            this.node.removeAllChildren();
             this.startIssueInsenseButtonNode.getComponent("startIssuseInsenseController").show()
         }
     }
 
     startBurn() {
-        this.initState(new Insense("incense-001", 60, "intelligence", 15, 0));
+        // this.initStateSingle(new Insense("incense-001", 10, "intelligence", 15, 0));
         this._burning = true;
         console.log("start burning");
     }
@@ -115,21 +188,21 @@ export class burningController extends Component {
     }   
 
     setInsenseModel(insense: Insense) {
-        if (this.insense.length >= 3) {
-            return 
-        }
+        // if (this.insense.length >= 3) {
+        //     return 
+        // }
 
-        let found = false
-        for (let i = 0; i < this.insense.length; i++) {
-            if (this.insense[i].getInsenseId() === insense.getInsenseId()) {
-                this.insense[i] = insense;
-                found = true;
-            }
-        }
-        if (!found) {
-            this.insense.push(insense);
-        }
-        this.initState(insense);
+        // let found = false
+        // for (let i = 0; i < this.insense.length; i++) {
+        //     if (this.insense[i].getInsenseId() === insense.getInsenseId()) {
+        //         this.insense[i] = insense;
+        //         found = true;
+        //     }
+        // }
+        // if (!found) {
+        //     this.insense.push(insense);
+        // }
+        this.initStateSingle(insense);
     }
 
     getInsenseModel(): Insense[] {
--- assets/scripts/game.ts
+++ assets/scripts/game.ts
@@ -1,6 +1,9 @@
 import { _decorator, CCInteger, Component, Node } from 'cc';
 import { UserInfo } from './model/userInfo';
+import "miniprogram-api-typings";
+
 const { ccclass, property } = _decorator;
+
 
 @ccclass('game')
 export class game extends Component {
@@ -22,6 +25,42 @@ export class game extends Component {
 
     start() {
         console.log("game start");
+        wx.showShareMenu({
+            menus : ['shareAppMessage', 'shareTimeline'],
+        });
+
+        wx.getSetting({
+            success(res) {
+              if (res.authSetting['scope.userInfo'] === true) {
+                wx.getUserInfo({
+                  success: (res) => {
+                    // 已经授权，直接获取用户信息
+                    console.log("用户信息", res)
+                  },
+                });
+              } else {
+                const button = wx.createUserInfoButton({
+                  type: "image",
+                  style: {
+                    left: 100,
+                    top: 100,
+                    width: 100,
+                    height: 100,
+                    backgroundColor: "rgba(255, 255, 255, 0.5)",
+                  },
+                });
+                button.onTap((res) => {
+                  if (res.errMsg.indexOf(':ok') > -1 && !!res.rawData) {
+                    // 获取用户信息
+                    console.log("用户信息", res)
+                  }
+                });
+              }
+            },
+          });
+
+          
+
         this.userInfoModel = this.getInitializedUserInfoModel();
         this.staticGroupNode.getComponent("staticsController").setUserInfoModel(this.userInfoModel);
         this.insenseUINode.getComponent("burningController").setUserInfoModel(this.userInfoModel);
